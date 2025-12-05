a384b767bf58e3b567aa1c396caab1ce8b960a33
Shubham Halder <shubham.halder@intglobal.com>
2025-12-05 19:13:00 +0530

    update dockling

--- ai/docling.py
+++ ai/docling.py
@@ -1,6 +1,7 @@
-from docling.document_converter import DocumentConverter
+#from docling.document_converter import DocumentConverter
 
 async def exract_markdown(file_path: str) -> str:
-    converter = DocumentConverter()
-    doc = converter.convert(file_path).document
-    return doc.export_to_markdown()
+    #converter = DocumentConverter()
+    #doc = converter.convert(file_path).document
+    #return doc.export_to_markdown()
+    return file_path
